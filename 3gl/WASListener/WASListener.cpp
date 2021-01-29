d5c6db2b7646da6ab0b316a579d5a2ebaff32c5b
uf-mtaylor <74602476+uf-mtaylor@users.noreply.github.com>
2021-01-29 15:47:35 +0100

    Bug/error handling (#1)
    * Improve error handling from the threads
    
    * Close listerner if there is a failure in the threads

--- 3gl/WASListener/WASListener.cpp
+++ 3gl/WASListener/WASListener.cpp
@@ -24,7 +24,7 @@ WCHAR szWindowClass[MAX_LOADSTRING]; // the main window class name
 
 // Forward declarations of functions included in this code module:
 ATOM MyRegisterClass(HINSTANCE hInstance) noexcept;
-BOOL InitInstance(HINSTANCE, int) noexcept;
+HWND InitInstance(HINSTANCE, int) noexcept;
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM) noexcept;
 void ShowContextMenu(HWND, POINT) noexcept;
@@ -46,7 +46,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE const hInstance,
 	MyRegisterClass(hInstance);
 
 	// Perform application initialization:
-	if (!InitInstance(hInstance, nShowCmd))
+	HWND hWnd = InitInstance(hInstance, nShowCmd);
+	if ( !hWnd )
 	{
 		return -1;
 	}
@@ -72,28 +73,41 @@ int APIENTRY wWinMain(_In_ HINSTANCE const hInstance,
 	// Setup our thread for processing the WorkArea updates in Uniface
 	auto workArea_promise{ std::make_shared<std::promise<std::wstring>>() }; // Uniface will return the location of the WorkArea
 
-	workerThreads[0] = std::thread([workArea_promise, uniface{ std::make_unique<CUniface>(std::move(commandLine), list) }] ()
+	workerThreads[0] = std::thread([hWnd, workArea_promise, uniface{ std::make_unique<CUniface>(std::move(commandLine), list) }] ()
 	{
 		// Getting the working folder will make a call to Uniface so starting the whole Uniface environment
-		workArea_promise->set_value(uniface->getWASFolder());
-		uniface->run();
+		try {
+			workArea_promise->set_value(uniface->getWASFolder());
+			uniface->run();
+		}
+		catch (const std::exception& e)
+		{
+			MessageBoxA(nullptr, e.what(), "Error", MB_OK);
+			workArea_promise->set_value(std::wstring());
+			PostMessage(hWnd, UM_DESTROY, 0, 0);
+
+		}
+
 		std::cout << "Finished processing actions\n";
 	});
 
 	// Setup our thread for watching the WorkArea
 	watcher = std::make_unique<CFolderWatcher>(list);
-	workerThreads[1] = std::thread([workArea_promise{ std::move(workArea_promise) }]()
+	workerThreads[1] = std::thread([hWnd, workArea_promise{ std::move(workArea_promise) }]()
 	{
 		try
 		{
 			// Wait for Uniface to return the location of the WorkArea
-			watcher->setFolder(workArea_promise->get_future().get());
+			auto workAreaFolder = workArea_promise->get_future().get();
+			if ( !workAreaFolder.empty() ) {
+				watcher->run(std::move(workAreaFolder)); // Start listening for changes in the WorkArea
+			}
 		}
 		catch (const std::exception& e)
 		{
 			MessageBoxA(nullptr, e.what(), "Error", MB_OK);
+			PostMessage(hWnd, UM_DESTROY, 0, 0 );
 		}
-		watcher->run(); // Start listening for changes in the WorkArea
 		std::cout << "Finished watching\n";
 	});
 
@@ -101,6 +115,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE const hInstance,
 	// Main message loop:
 	while (GetMessage(&msg, nullptr, 0, 0))
 	{
+
 		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
 		{
 			TranslateMessage(&msg);
@@ -147,12 +162,12 @@ ATOM MyRegisterClass(HINSTANCE hInstance) noexcept
 //        In this function, we save the instance handle in a global variable and
 //        create and display the main program window.
 //
-BOOL InitInstance(HINSTANCE const hInstance, int) noexcept
+HWND InitInstance(HINSTANCE const hInstance, int) noexcept
 {
 	hInst = hInstance; // Store instance handle in our global variable
 
 	return CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr) ? TRUE : FALSE;
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 }
 
 void ShowContextMenu(HWND const hwnd, POINT const pt) noexcept
@@ -258,6 +273,11 @@ LRESULT CALLBACK WndProc(HWND const hWnd, UINT const message, WPARAM const wPara
 			EndPaint(hWnd, &ps);
 		}
 		break;
+
+	case UM_DESTROY: 
+		DestroyWindow(hWnd);
+		return TRUE;
+
 	case WM_DESTROY:
 		// Stop the processing
 		list->close();
